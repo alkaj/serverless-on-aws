1f44edc83420ae82a613ea04ba3578af9c626f87
Kamla Junior <alkaj09@gmail.com>
2021-11-11 23:33:51 +0100

    Huge initial project with all working and logs ok

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,33 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { deleteTodo } from '../../helpers/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // TODO: Remove a TODO item by id
+    const deleted = await deleteTodo(getUserId(event), todoId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        todoId: deleted
+      })
+    }
+  }
+)
+
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true
+  })
+)
